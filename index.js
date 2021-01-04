0c53c3a8c26d23a88f78438c96054448951deb43
郑瑞 <rui.zheng@medbanks.cn>
2021-01-04 16:14:55 +0800

    feat

---
+++ index.js
@@ -0,0 +1,47 @@
+let fs = require('fs');
+const hmacSHA256 =require('./build/hmac-sha512');
+// const Base64 =require('./build/enc-base64');
+
+
+
+let cleanedData=null;
+//同步读文件
+try{
+  let readStrData=fs.readFileSync('/Users/zhengrui/Desktop/learn/crypto-js/clean_data.txt','utf8');
+  cleanedData=dataHandle(readStrData);
+}catch(err){
+  console.log(err);
+}
+
+
+/* 数据处理后写成文件 */
+function dataHandle(data){
+  //字符串
+  console.log(data)
+  let arr=data&&data.split('\r\n');
+  console.log(arr)
+  //转化为数组
+  let passwordArr=[];
+   for(let i=0;i<arr.length;i++){
+    var hash = hmacSHA256(arr[i], "spcare").toString();
+    // console.log(`${arr[i]}+${hash}`)
+    passwordArr.push(`${arr[i]}:${hash}`)
+   }
+   //处理后的需要数据
+  let strData=passwordArr.join('\n ');
+  console.log(strData)
+  return strData
+}
+
+//参数：路径 写入内容 返回函数
+fs.writeFile('/Users/zhengrui/Desktop/learn/crypto-js/password.txt',cleanedData,(err)=>{
+  if(!err){
+    console.log(cleanedData)
+    console.log('成功~');
+  }else{
+    console.log(err);
+  };
+ });
+
+
+
